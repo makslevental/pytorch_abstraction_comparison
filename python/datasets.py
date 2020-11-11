ff894271374776a76d984353fb869493db05c11c
elena-orlova <egorlova68@gmail.com>
2020-11-10 18:46:58 -0600

    smaller model trains so I guess resnet is too high capacity (should replace stl then)

--- python/datasets.py
+++ python/datasets.py
@@ -1,24 +1,30 @@
 import os
 import numpy as np
 
-from torchvision.datasets import FashionMNIST, STL10, VOCDetection, MNIST
+from torchvision.datasets import FashionMNIST, STL10, VOCDetection, MNIST, CIFAR10
 
-mnist_train = MNIST("../data", train=True, download=True)
-mnist_test = MNIST("../data", train=False, download=True)
+# mnist_train = MNIST("../data", train=True, download=True)
+# mnist_test = MNIST("../data", train=False, download=True)
+#
+# fashion_mnist_train = FashionMNIST("../data", train=True, download=True)
+# fashion_mnist_test = FashionMNIST("../data", train=False, download=True)
+#
+# stl_10_train = STL10("../data", split="train", download=True)
+# data, target = stl_10_train[0]
+# if not os.path.exists("../data/stl_10_train_data.npy"):
+#     np.save("../data/stl_10_train_data.npy", stl_10_train.data)
+# if not os.path.exists("../label/stl_10_train_labels.npy"):
+#     np.save("../data/stl_10_train_labels.npy", stl_10_train.labels)
+# stl_10_test = STL10("../data", split="test", download=True)
+# if not os.path.exists("../data/stl_10_test_data.npy"):
+#     np.save("../data/stl_10_test_data.npy", stl_10_test.data)
+# if not os.path.exists("../label/stl_10_test_labels.npy"):
+#     np.save("../data/stl_10_test_labels.npy", stl_10_test.labels)
+#
+# pascal_train = VOCDetection("../data", year="2012", image_set="train", download=True)
+# pascal_test = VOCDetection("../data", year="2012", image_set="val", download=True)
 
-fashion_mnist_train = FashionMNIST("../data", train=True, download=True)
-fashion_mnist_test = FashionMNIST("../data", train=False, download=True)
+cifar_10_train = CIFAR10("../data", train=True, download=True)
+cifar_10_test = CIFAR10("../data", train=False, download=True)
 
-stl_10_train = STL10("../data", split="train", download=True)
-if not os.path.exists("../data/stl_10_train_data.npy"):
-    np.save("../data/stl_10_train_data.npy", stl_10_train.data)
-if not os.path.exists("../label/stl_10_train_labels.npy"):
-    np.save("../data/stl_10_train_labels.npy", stl_10_train.labels)
-stl_10_test = STL10("../data", split="test", download=True)
-if not os.path.exists("../data/stl_10_test_data.npy"):
-    np.save("../data/stl_10_test_data.npy", stl_10_test.data)
-if not os.path.exists("../label/stl_10_test_labels.npy"):
-    np.save("../data/stl_10_test_labels.npy", stl_10_test.labels)
-
-pascal_train = VOCDetection("../data", year="2012", image_set="train", download=True)
-pascal_test = VOCDetection("../data", year="2012", image_set="val", download=True)
+print()
