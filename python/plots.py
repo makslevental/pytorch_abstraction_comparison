21efe4c492be3853236ddf07b476d1dc68dc8518
makslevental <maksim.levental@gmail.com>
2020-12-05 23:35:45 -0600

    results almost done

--- python/plots.py
+++ python/plots.py
@@ -35,7 +35,7 @@ def cleanup_profiles_cudnn():
                         if "SUMMARY" in l
                     ],
                     enumerate(
-                        sorted(glob.glob(f"profiles/cudnn/run*{impl}_{dataset}_*.csv"))
+                        sorted(glob.glob(f"profiles/cudnn2/run*{impl}_{dataset}_*.csv"))
                     ),
                     [],
                 )
@@ -49,7 +49,7 @@ def cleanup_profiles_cudnn():
                         if "EVAL" in l
                     ],
                     enumerate(
-                        sorted(glob.glob(f"profiles/cudnn/run*{impl}_{dataset}_*.csv"))
+                        sorted(glob.glob(f"profiles/cudnn2/run*{impl}_{dataset}_*.csv"))
                     ),
                     [],
                 )
@@ -187,7 +187,7 @@ def plot(
     label="cudnn",
     title="Average train loss per epoch",
     ylabel="loss",
-    shift=np.abs(np.random.normal(0, 1e-7, 100)),
+    shift=np.abs(np.random.normal(0, 1e-70, 100)),
 ):
     fig, ax = fig_ax
     if fig is None or ax is None:
@@ -196,7 +196,34 @@ def plot(
     ax.legend()
     ax.set_title(title)
     ax.set_ylabel(ylabel)
-    ax.set_xlabel("epoch")
+    ax.set_xlabel("epoch/time")
+    ax.set_yscale("log")
+    ax.fill_between(times, min + shift, max + shift, alpha=0.2)
+
+    return fig, ax
+
+
+def scatter(
+    min,
+    mean,
+    max,
+    times,
+    fig_ax=None,
+    label="cudnn",
+    title="Average train loss per epoch",
+    ylabel="loss",
+    xlabel="",
+    shift=np.abs(np.random.normal(0, 1e-70, 100)),
+):
+    times = np.log2(times)
+    fig, ax = fig_ax
+    if fig is None or ax is None:
+        fig, ax = plt.subplots()
+    ax.plot(times, mean + shift, "-", label=label)
+    ax.legend()
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
     ax.set_yscale("log")
     ax.fill_between(times, min + shift, max + shift, alpha=0.2)
 
@@ -209,12 +236,15 @@ def get_min_mean_max(dfs, impl, dataset, train=True):
 
 
 def plot_all(profile_dfs, resolution_dfs: dict):
+    impls = ["cudnn", "libtorch", "pytorch", "torchscript"]
     input_list = []
     for key in ["accuracy", "avg loss"]:
         for train in [True, False]:
             for dataset in datasets:
                 fig = ax = None
                 for impl in impls:
+                    if ("train" if train else "eval") not in profile_dfs[impl][dataset]:
+                        continue
                     min, mean, max = get_min_mean_max(
                         profile_dfs, impl, dataset, train=train
                     )
@@ -222,31 +252,33 @@ def plot_all(profile_dfs, resolution_dfs: dict):
                         min[key].values,
                         mean[key].values,
                         max[key].values,
-                        mean["avg sample time"].values.cumsum(),
+                        mean["avg sample time"].values.cumsum() if train else list(range(100)),
                         (fig, ax),
                         label=impl,
                         title=f"{'train' if train else 'eval'} {key} per epoch {dataset}",
                         ylabel=key,
                     )
-                plt.show()
-                # tikzplotlib.clean_figure()
-                # tfp = f"../tex/{'train' if train else 'eval'} {key} per epoch {dataset}.tex".replace(
-                #     " ", "_"
-                # )
-                # input_list.append(tfp)
-                # tikzplotlib.save(tfp, standalone=False)
+                # plt.show()
+                tikzplotlib.clean_figure()
+                tfp = f"../tex/figures/plots/{'train' if train else 'eval'} {key} per epoch {dataset}.tex".replace(
+                    " ", "_"
+                )
+                input_list.append(tfp)
+                tikzplotlib.save(tfp, standalone=False)
 
     # units = {
     #     "avg sample time": "time (ms)",
     #     "avg used mem": "memory (mb)",
     #     "avg gpu util": "util (\%)",
     # }
-    # log = {"zmode=log, log origin=infty"}
     # batch_sizes = np.power(2, list(range(3, 9 + 1)))
     # resolutions = np.power(2, list(range(3, 12 + 1)))
     # for train in ["train", "eval"]:
     #     for key in ["avg sample time", "avg used mem", "avg gpu util"]:
     #         for impl in impls:
+    #             if train not in resolution_dfs[impl]:
+    #                 continue
+    #
     #             points = []
     #             for i, batch_size in enumerate(batch_sizes):
     #                 for j, resolution in enumerate(resolutions):
@@ -265,12 +297,17 @@ def plot_all(profile_dfs, resolution_dfs: dict):
     #                         )
     #             if points:
     #                 csv.writer(
-    #                     open(f"../tex/{impl}_{train}_{key}.csv".replace(" ", "_"), "w"),
+    #                     open(
+    #                         f"../tex/figures/plots/{impl}_{train}_{key}.csv".replace(
+    #                             " ", "_"
+    #                         ),
+    #                         "w",
+    #                     ),
     #                     delimiter=" ",
     #                 ).writerows(points)
-    #         gfp = f"../tex/{train}_{key}.tex".replace(" ", "_")
+    #         gfp = f"../tex/figures/plots/{train}_{key}.tex".replace(" ", "_")
     #         input_list.append(gfp)
-    #         with open("../tex/scatter_tex") as f, open(gfp, "w") as g:
+    #         with open("../tex/figures/plots/scatter_tex") as f, open(gfp, "w") as g:
     #             text = replace_all(
     #                 f.read(),
     #                 {
@@ -289,21 +326,124 @@ def plot_all(profile_dfs, resolution_dfs: dict):
     #                 },
     #             )
     #             g.write(text)
+    # units = {
+    #     "avg sample time": "time (ms)",
+    #     "avg used mem": "memory (mb)",
+    #     "avg gpu util": "util (\%)",
+    # }
+    # batch_sizes = [32]
+    # resolutions = np.power(2, list(range(3, 12 + 1)))
+    # for train in ["train", "eval"]:
+    #     for key in ["avg sample time", "avg used mem", "avg gpu util"]:
+    #         fig = ax = None
+    #         for impl in impls + ["torchscript"]:
+    #             points = []
+    #             for i, batch_size in enumerate(batch_sizes):
+    #                 for j, resolution in enumerate(resolutions):
+    #                     if (
+    #                         batch_size in resolution_dfs[impl][train]
+    #                         and resolution in resolution_dfs[impl][train][batch_size]
+    #                     ):
+    #                         points.append(
+    #                             (
+    #                                 batch_size,
+    #                                 resolution,
+    #                                 resolution_dfs[impl][train][batch_size][resolution][
+    #                                     key
+    #                                 ].values,
+    #                             )
+    #                         )
+    #             if points:
+    #                 fig, ax = scatter(
+    #                     [x[2].min() for x in points],
+    #                     [x[2].mean() for x in points],
+    #                     [x[2].max() for x in points],
+    #                     [x[1] for x in points],
+    #                     (fig, ax),
+    #                     label=impl,
+    #                     title=f"batch size {batch_size} {key} for resolutions",
+    #                     ylabel=units[key],
+    #                     xlabel="resolution",
+    #                     shift=np.abs(np.random.normal(0, 1e-70, len(points))),
+    #                 )
+    #
+    #         # plt.show()
+    #         tikzplotlib.clean_figure()
+    #         tfp = f"../tex/figures/plots/batch size {batch_size} {key} for resolutions.tex".replace(
+    #             " ", "_"
+    #         )
+    #         input_list.append(tfp)
+    #         tikzplotlib.save(tfp, standalone=False)
+    #
+    # batch_sizes = np.power(2, list(range(3, 9 + 1)))
+    # resolutions = [32]
+    # for train in ["train", "eval"]:
+    #     for key in ["avg sample time", "avg used mem", "avg gpu util"]:
+    #         fig = ax = None
+    #         for impl in impls + ["torchscript"]:
+    #             points = []
+    #             for i, batch_size in enumerate(batch_sizes):
+    #                 for j, resolution in enumerate(resolutions):
+    #                     if (
+    #                         batch_size in resolution_dfs[impl][train]
+    #                         and resolution in resolution_dfs[impl][train][batch_size]
+    #                     ):
+    #                         points.append(
+    #                             (
+    #                                 batch_size,
+    #                                 resolution,
+    #                                 resolution_dfs[impl][train][batch_size][resolution][
+    #                                     key
+    #                                 ].values,
+    #                             )
+    #                         )
+    #             if points:
+    #                 fig, ax = scatter(
+    #                     [x[2].min() for x in points],
+    #                     [x[2].mean() for x in points],
+    #                     [x[2].max() for x in points],
+    #                     [x[0] for x in points],
+    #                     (fig, ax),
+    #                     label=impl,
+    #                     title=f"resolution {resolution} {key} for batch size",
+    #                     ylabel=units[key],
+    #                     xlabel="batch size",
+    #                     shift=np.abs(np.random.normal(0, 1e-70, len(points))),
+    #                 )
+    #         # plt.show()
+    #         tikzplotlib.clean_figure()
+    #         tfp = f"../tex/figures/plots/resolution {resolution} {key} for batch size.tex".replace(
+    #             " ", "_"
+    #         )
+    #         input_list.append(tfp)
+    #         tikzplotlib.save(tfp, standalone=False)
+    #
+    #
     # u = """
     # \\begin{{figure}}
     #     {tikz}
     # \end{{figure}}
     # """
-    # with open("../tex/all_plots_tex") as tf, open("../tex/all_plots.tex", "w") as uf:
+    # with open("../tex/figures/plots/all_plots_tex") as tf, open(
+    #     "../tex/figures/plots/all_plots.tex", "w"
+    # ) as uf:
     #     text = tf.read()
-    #     uf.write(text.replace(
-    #         "{input_list}",
-    #         "\n".join([u.format(tikz=f"\input{{{i}}}".replace("../tex/", "")) for i in input_list])
-    #     ))
+    #     uf.write(
+    #         text.replace(
+    #             "{input_list}",
+    #             "\n".join(
+    #                 [
+    #                     u.format(tikz=f"\input{{{i}}}".replace("../tex/", ""))
+    #                     for i in input_list
+    #                 ]
+    #             ),
+    #         )
+    #     )
+
 
 if __name__ == "__main__":
     # cleanup_profiles()
-    # cleanup_profiles_cudnn()
+    cleanup_profiles_cudnn()
     # cleanup_resolutions()
     profile_dfs = make_dfs()
     resolution_dfs = make_resolution_dfs()
