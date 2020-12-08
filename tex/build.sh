eeec56b4c0d99a2af64bc509a004a97fd7477d9d
makslevental <maksim.levental@gmail.com>
2020-12-07 23:55:01 -0600

    discussion section

--- tex/build.sh
+++ tex/build.sh
@@ -1,4 +1,4 @@
 cd /Users/maksim/dev_projects/pytorch_abstraction_comparison/tex
 source venv/bin/activate
-pdflatex -shell-escape poster; bibtex poster; pdflatex -shell-escape poster; pdflatex -shell-escape poster
-#pdflatex -shell-escape main; bibtex main; pdflatex -shell-escape main; pdflatex -shell-escape main
+#pdflatex -shell-escape poster; bibtex poster; pdflatex -shell-escape poster; pdflatex -shell-escape poster
+pdflatex -shell-escape main; bibtex main; pdflatex -shell-escape main; pdflatex -shell-escape main
