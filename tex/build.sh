7842c5b396b68082373cbdb3f3d2008f423ba410
elena-orlova <egorlova68@gmail.com>
2020-12-11 15:47:31 -0600

    add call graph and future work

--- tex/build.sh
+++ tex/build.sh
@@ -1,4 +1,5 @@
 cd /Users/maksim/dev_projects/pytorch_abstraction_comparison/tex
 source venv/bin/activate
 #pdflatex -shell-escape poster; bibtex poster; pdflatex -shell-escape poster; pdflatex -shell-escape poster
+rm *.aux *.bbl *.out *.out
 pdflatex -shell-escape main; bibtex main; pdflatex -shell-escape main; pdflatex -shell-escape main
